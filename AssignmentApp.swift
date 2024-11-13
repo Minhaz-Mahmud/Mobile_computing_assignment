5dc9b1f1d8a4a712d7947998ec52a5bfc1ce0d94
Minhaz Mahmud Mahadi <mmahmud25dec@gmail.com>
2024-11-13 19:10:13 +0600

    added

---
+++ AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by Mahir Azmain Haque on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
